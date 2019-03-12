49d04f436dda9fea7ceac1e293c4e7ad9d9c00a9
kushalvith <43214905+kushalvith@users.noreply.github.com>
2019-03-12 18:30:07 +0530

    Merge branch 'master' of D:\Voting-System with conflicts.

--- System/app/src/main/java/com/example/votingsystem/Candidate_Add_Activity.java
+++ System/app/src/main/java/com/example/votingsystem/Candidate_Add_Activity.java
@@ -1,5 +1,7 @@
 package com.example.votingsystem;
 
+import android.content.Intent;
+import android.nfc.Tag;
 import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
@@ -7,18 +9,18 @@ import android.util.Log;
 import android.view.View;
 import android.widget.EditText;
 
+import com.google.android.gms.tasks.OnCompleteListener;
 import com.google.android.gms.tasks.OnFailureListener;
 import com.google.android.gms.tasks.OnSuccessListener;
 import com.google.firebase.firestore.DocumentReference;
+import com.google.firebase.firestore.DocumentSnapshot;
 import com.google.firebase.firestore.FirebaseFirestore;
 
 import java.util.HashMap;
 import java.util.Map;
 
-public class Candidate_Add_Activity extends AppCompatActivity {
 
-    FirebaseFirestore db = FirebaseFirestore.getInstance();
-    protected static final String ass = "Done";
+public class Candidate_Add_Activity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -26,31 +28,39 @@ public class Candidate_Add_Activity extends AppCompatActivity {
         setContentView(R.layout.activity_add_candidate);
     }
 
-    public void send_data(View view)
-    {
+    FirebaseFirestore db = FirebaseFirestore.getInstance();
+    protected static final String TAG = "Done";
+
+    public void adddata(View view) {
+
         EditText Name = findViewById(R.id.editText4);
-        EditText Party = findViewById(R.id.editText5);
-        EditText Des = findViewById(R.id.editText6);
+        EditText PartyName = findViewById(R.id.editText5);
+        EditText Description = findViewById(R.id.editText6);
 
-        String name = Name.getText().toString();
-        String party = Party.getText().toString();
-        String des = Des.getText().toString();
+        String cname = Name.getText().toString();
+        String Partyname = PartyName.getText().toString();
+        String Desc = Description.getText().toString();
+        Map<String, Object> dataTosave = new HashMap<>();
+        dataTosave.put("Name", cname);
+        dataTosave.put("Partyname", Partyname);
+        dataTosave.put("Description", Desc);
 
-        Map<String,Object> dataToSave = new HashMap<>();
-        dataToSave.put("Name",name);
-        dataToSave.put("Party",party);
-        dataToSave.put("Description",des);
-
-        db.collection("Jaival").add(dataToSave).addOnSuccessListener(new OnSuccessListener<DocumentReference>() {
+        db.collection("Kushal").add(dataTosave).addOnSuccessListener(new OnSuccessListener<DocumentReference>() {
             @Override
             public void onSuccess(DocumentReference documentReference) {
-                Log.d(ass, "Data Fetched");
+                Log.d(TAG, "Data loaded");
+
             }
+
         }).addOnFailureListener(new OnFailureListener() {
             @Override
             public void onFailure(@NonNull Exception e) {
-                Log.w(ass, "Error", e);
+                Log.w(TAG, "failure", e);
+
+
             }
         });
+
+
     }
 }
--- System/app/src/main/java/com/example/votingsystem/MainActivity.java
+++ System/app/src/main/java/com/example/votingsystem/MainActivity.java
@@ -1,17 +1,26 @@
 package com.example.votingsystem;
 
 import android.content.Intent;
+import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
+import android.widget.Button;
+
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.firestore.DocumentReference;
+import com.google.firebase.firestore.FirebaseFirestore;
+
+import java.util.HashMap;
+import java.util.Map;
 
 public class MainActivity extends AppCompatActivity {
 
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
-    }
+
 
     public void goToAdmin(View view)
     {
