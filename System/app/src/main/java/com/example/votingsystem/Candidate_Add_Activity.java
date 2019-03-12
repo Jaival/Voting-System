eeccf3625cc1d0ed7389d769ea18fdeeccdf3583
kushalvith <43214905+kushalvith@users.noreply.github.com>
2019-03-12 21:28:10 +0530

    UI-Changed

--- System/app/src/main/java/com/example/votingsystem/Candidate_Add_Activity.java
+++ System/app/src/main/java/com/example/votingsystem/Candidate_Add_Activity.java
@@ -1,6 +1,9 @@
 package com.example.votingsystem;
+
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 
 public class Candidate_Add_Activity extends AppCompatActivity {
 
--- System/app/src/main/java/com/example/votingsystem/Candidate_Edit_Activity.java
+++ System/app/src/main/java/com/example/votingsystem/Candidate_Edit_Activity.java
@@ -1,44 +1,12 @@
 package com.example.votingsystem;
-
-import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
 
 public class Candidate_Edit_Activity extends AppCompatActivity {
-
-//    FirebaseFirestore db = FirebaseFirestore.getInstance();
-//    public DocumentReference docRef = db.collection("Jaival").document("");
-//    protected static final String ass = "Done";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_candidate_edit);
     }
-
-//    public void fetch(View view)
-//    {
-//        TextView Name = findViewById(R.id.textView7);
-//        TextView Party = findViewById(R.id.textView8);
-//        TextView Des = findViewById(R.id.textView9);
-//        docRef.get().addOnCompleteListener(new OnCompleteListener<DocumentSnapshot>() {
-//            @Override
-//            public void onComplete(@NonNull Task<DocumentSnapshot> task) {
-//                if(task.isSuccessful())
-//                {
-//                    DocumentSnapshot document = task.getResult();
-//                    if(document.exists())
-//                    {
-//                        Log.d(ass,"Done"+ document.getData());
-//                    }
-//                    else
-//                    {
-//                        Log.d(ass,"No Such Document");
-//                    }
-//                }
-//                else
-//                {
-//                    Log.d(ass,"Data Failed To Fetch");
-//                }
-//            }
-//        });
 }
