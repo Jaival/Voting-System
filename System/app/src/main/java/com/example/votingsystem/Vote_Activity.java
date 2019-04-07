382f2517e7ef1dd0de84c43c18e81adedf9cd24d
Jaival <saijajaival@gmail.com>
2019-04-07 10:20:02 +0530

    Radio Button Functionality Added

--- System/app/src/main/java/com/example/votingsystem/Vote_Activity.java
+++ System/app/src/main/java/com/example/votingsystem/Vote_Activity.java
@@ -1,51 +1,66 @@
 package com.example.votingsystem;
 
 import android.content.Intent;
+import android.location.Criteria;
 import android.os.Bundle;
 import android.support.annotation.IdRes;
+import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
 import android.view.View;
 import android.widget.RadioButton;
 import android.widget.RadioGroup;
+import android.widget.Toast;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.firestore.DocumentReference;
+import com.google.firebase.firestore.DocumentSnapshot;
+import com.google.firebase.firestore.FirebaseFirestore;
+
+import java.util.HashMap;
+import java.util.Map;
 
 public class Vote_Activity extends AppCompatActivity {
-  //  DocumentReference demoRef = FirebaseFirestore.getInstance();
-    protected String candidate;
+    DatabaseReference rootRef = FirebaseDatabase.getInstance().getReference();
+    DatabaseReference condRef = rootRef.child("Candidate");
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_vote);
+        canrad();
     }
 
     public void canrad() {
-        RadioGroup rg = findViewById(R.id.radioGroup2);
-        RadioButton votebtn = findViewById(rg.getCheckedRadioButtonId());
-        candidate = votebtn.getText().toString();
-
+        final RadioGroup rg = findViewById(R.id.radioGroup2);
         rg.setOnCheckedChangeListener(new RadioGroup.OnCheckedChangeListener() {
             @Override
-            public void onCheckedChanged(RadioGroup radioGroup, @IdRes int i) {
-                switch (i) {
-                    case R.id.radioButton2:
-                        candidate = "Candidate 1";
-                        break;
-                    case R.id.radioButton3:
-                        candidate = "Candidate 2";
-                        break;
-                    case R.id.radioButton4:
-                        candidate = "Candidate 3";
-                        break;
-                    case R.id.radioButton5:
-                        candidate = "Candidate 4";
-                        break;
+            public void onCheckedChanged(RadioGroup group, int checkedId) {
+                if (checkedId == R.id.candi_1) {
+                    Toast.makeText(Vote_Activity.this, "Candidate 1 selected!", Toast.LENGTH_LONG).show();
+                    condRef.setValue("1");
+                } else if (checkedId == R.id.candi_2) {
+                    Toast.makeText(Vote_Activity.this, "Candidate 2 selected!", Toast.LENGTH_LONG).show();
+                    condRef.setValue("2");
+                } else if (checkedId == R.id.candi_3) {
+                    Toast.makeText(Vote_Activity.this, "Candidate 3 selected!", Toast.LENGTH_LONG).show();
+                    condRef.setValue("3");
+                } else if (checkedId == R.id.candi_4) {
+
+                    Toast.makeText(Vote_Activity.this, "Candidate 4 selected!", Toast.LENGTH_LONG).show();
+                    condRef.setValue("4");
                 }
+                rg.getCheckedRadioButtonId();
             }
         });
     }
 
-    public void gotomainActivity(View view) {
-
+    public void gotomainActivity(View view){
         Intent intent = new Intent(this, MainActivity.class);
         startActivity(intent);
     }
