87defaa3de8f91c65706830505fef1aeb808f0e5
Vanshika <shahvanshika61@gmail.com>
2019-04-26 19:23:26 +0530

    voting altered

--- System/app/src/main/java/com/example/votingsystem/Vote_Activity.java
+++ System/app/src/main/java/com/example/votingsystem/Vote_Activity.java
@@ -1,15 +1,12 @@
 package com.example.votingsystem;
 
 import android.content.Intent;
-import android.location.Criteria;
 import android.os.Bundle;
-import android.support.annotation.IdRes;
 import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
 import android.view.View;
 import android.widget.Button;
-import android.widget.RadioButton;
 import android.widget.RadioGroup;
 import android.widget.Toast;
 
@@ -27,33 +24,48 @@ import java.util.HashMap;
 import java.util.Map;
 
 public class Vote_Activity extends AppCompatActivity {
+    DatabaseReference rootRef = FirebaseDatabase.getInstance().getReference();
+    DatabaseReference condRef = rootRef.child("Candidate");
     FirebaseFirestore db = FirebaseFirestore.getInstance();
-    protected static final String TAG = "Done";
+
+    final String TAG="Done";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_vote);
         canrad();
+        final Button btn = (Button) findViewById(R.id.Vote);
+        btn.setEnabled(false);
     }
 
-    public void canrad() {
+    public void canrad(){
         final RadioGroup rg = findViewById(R.id.radioGroup2);
+        final Button btn = (Button) findViewById(R.id.Vote);
         rg.setOnCheckedChangeListener(new RadioGroup.OnCheckedChangeListener() {
             @Override
             public void onCheckedChanged(RadioGroup group, int checkedId) {
                 if (checkedId == R.id.candi_1) {
                     Toast.makeText(Vote_Activity.this, "Candidate 1 selected!", Toast.LENGTH_LONG).show();
+                    condRef.setValue("1");
+                    btn.setEnabled(true);
 
                 } else if (checkedId == R.id.candi_2) {
                     Toast.makeText(Vote_Activity.this, "Candidate 2 selected!", Toast.LENGTH_LONG).show();
+                    condRef.setValue("2");
+                    btn.setEnabled(true);
 
                 } else if (checkedId == R.id.candi_3) {
                     Toast.makeText(Vote_Activity.this, "Candidate 3 selected!", Toast.LENGTH_LONG).show();
+                    condRef.setValue("3");
+                    btn.setEnabled(true);
 
                 } else if (checkedId == R.id.candi_4) {
 
                     Toast.makeText(Vote_Activity.this, "Candidate 4 selected!", Toast.LENGTH_LONG).show();
+                    condRef.setValue("4");
+                    btn.setEnabled(true);
 
                 }
                 rg.getCheckedRadioButtonId();
