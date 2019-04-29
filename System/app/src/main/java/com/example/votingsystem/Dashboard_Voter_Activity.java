9e3564a757a605f377fdf055ca13a98dc46f7492
Jaival <saijajaival@gmail.com>
2019-04-29 18:10:03 +0530

    Authentication Added

--- System/app/src/main/java/com/example/votingsystem/Dashboard_Voter_Activity.java
+++ System/app/src/main/java/com/example/votingsystem/Dashboard_Voter_Activity.java
@@ -7,6 +7,7 @@ import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.Button;
 
+import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.database.DataSnapshot;
 import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
@@ -80,4 +81,19 @@ public class Dashboard_Voter_Activity extends AppCompatActivity {
             }
         });
     }
+
+    public void Logout(View view) {
+        findViewById(R.id.Logout).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                FirebaseAuth.getInstance().signOut();
+
+                Intent intent = new Intent(Dashboard_Voter_Activity.this, MainActivity.class);
+                intent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK | Intent.FLAG_ACTIVITY_CLEAR_TASK);
+
+                startActivity(intent);
+            }
+        });
+    }
+
 }
--- System/app/src/main/java/com/example/votingsystem/MainActivity.java
+++ System/app/src/main/java/com/example/votingsystem/MainActivity.java
@@ -4,22 +4,57 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
+import android.widget.EditText;
+
+import com.google.firebase.auth.FirebaseAuth;
 
 public class MainActivity extends AppCompatActivity {
+
+    private EditText editText;
 
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+//        Intent intent = new Intent(this, MainVerification.class);
+//        startActivity(intent);
+
+        editText = findViewById(R.id.EditTextNumber);
+
+        findViewById(R.id.login).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+                String number = editText.getText().toString().trim();
+
+                if (number.isEmpty() || number.length() < 10) {
+                    editText.setError("Valid number is required");
+                    editText.requestFocus();
+                    return;
+                }
+
+                String phoneNumber = "+" + "91" + number;
+
+                Intent intent = new Intent(MainActivity.this, MainVerification.class);
+                intent.putExtra("PhoneNumber", phoneNumber);
+                startActivity(intent);
+            }
+        });
     }
-    public void goToAdmin(View view)
-    {
-        Intent intent = new Intent(this, Dashboard_Admin_Activity.class);
+
+    public void goToAdminLogin(View view) {
+        Intent intent = new Intent(this, MainAdminLogin.class);
         startActivity(intent);
     }
 
-    public void goToVoters(View view)
-    {
-        Intent intent = new Intent(this, Dashboard_Voter_Activity.class);
-        startActivity(intent);
+    @Override
+    protected void onStart() {
+        super.onStart();
+
+        if (FirebaseAuth.getInstance().getCurrentUser() != null) {
+            Intent intent = new Intent(this, Dashboard_Voter_Activity.class);
+            intent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK | Intent.FLAG_ACTIVITY_CLEAR_TASK);
+            startActivity(intent);
+        }
     }
 }
